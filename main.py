458e176534866724377375adce8a02bdbe945252
Vinay-raja-mohan <mail2vinay2004@gmail.com>
2026-01-22 23:16:58 +0530

    Update code from Gen-AI IDE

---
+++ main.py
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<title>My Page</title>
+
+
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<h1>Hello World</h1>
+nd drnhe shj brujwb 
+
+
+<script src="script.js"></script>
+</body>
+</html>
