73311cb9c64e1146b9405e031a0e5d9e1e9e6ffa
Vinay-raja-mohan <mail2vinay2004@gmail.com>
2026-01-22 23:34:11 +0530

    Update code from Gen-AI IDE

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8" />
-<title>My Page</title>
-
-
-<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-<h1>Hello World</h1>
-
-
-<script src="script.js"></script>
-</body>
-</html>
+# Write your Python code here
+print("Hello World")
+jbyv
